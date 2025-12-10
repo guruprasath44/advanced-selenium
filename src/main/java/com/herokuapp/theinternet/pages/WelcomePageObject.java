1109dc3ab184f7f263547a92a71cd1374eed162f
guruprasath44 <guruprasathindirakumar@gmail.com>
2025-12-10 16:28:42 +0530

    Created dropdownpage

--- src/main/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
+++ src/main/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
@@ -8,6 +8,7 @@ public class WelcomePageObject extends BasePageObject{
 
     private final By formAuthenticationLinkLocator = By.linkText("Form Authentication");
     private By checkboxesLinkLocator = By.linkText("Checkboxes");
+    private By dropdownLinkLocator = By.linkText("Dropdown");
 
     public WelcomePageObject(WebDriver driver, Logger log) {
         super (driver, log );
@@ -32,5 +33,11 @@ public class WelcomePageObject extends BasePageObject{
         log.info("Clicking Checkboxes link on Welcome Page");
         click(checkboxesLinkLocator);
         return new CheckboxesPage(driver, log);
+    }
+
+    public DropdownPage clickDropdownLink() {
+        log.info("Clicking Dropdown link on Welcome Page");
+        click(dropdownLinkLocator);
+        return new DropdownPage(driver, log);
     }
 }
